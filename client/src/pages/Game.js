6d00b6da42eb2a29464b7ef104736f43a17e9955
Matthew Eisenstadter <mattieice@gmail.com>
2018-08-23 09:02:43 -0400

    merged game.js

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,57 +1,36 @@
 import React, { Component } from "react";
-import Keyboard from "../components/Keyboard";
+import axios from "axios";
 import firebase from "firebase";
 import Firebase, { auth, provider } from '../utils/Firebase';
-
+import Keyboard from "../components/Keyboard";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import Timer from "../components/Timer";
 import AnswerSpace from "../components/AnswerSpace";
-import UserWordValue from "../components/UserWordValue"; //for this round
+import UserWordValue from "../components/UserWordValue";
 import CurrentLevel from "../components/CurrentLevel";
 import TotalUserScore from "../components/TotalUserScore";
 import Card from "../components/Card";
-//import CardBtn from "../components/CardBtn";
-import API from "../utils/API.js";
-//Definition??
+import { EventEmitter } from "events";
+var wordList = require("categorized-words");
 
 
-// hide buttons until win/loss is triggered
-// create card buttons for play again and not (win: yes or no; loss: yes or no)
-// disable buttons on click of submit
-// start button to begin game if time (see trivia game)
-// on submit, trigger next card with score, result, definition (loadNextDog function!)
-// merge with dev
-// deploy to heroku
-// PREPARE PRESENTATION --
-    // tell a story: how you got there (challenges, how you've worked through them)
-    // use buzzwords: state, etc. -- JQuery to React how?
-    // focus more on demoing -- no powerpoint
-    // what we've taken from this class (how to juggle hw while working full-time)
 
-
-// for the API, do I even need to loop through all entries? can I just see if joinedArray returns any definition?????
-
-
-export default class Game extends React.Component{
-
-// class Game extends Component {
+class Game extends Component {
 
     partsOfSpeech = [
         "noun",
         "verb",
         "adjective"
     ];
-
-    // POSArray = Object.values(this.partsOfSpeech);
-    // abbreviatedPOSArray = Object.keys(this.partsOfSpeech);
+    
     constructor(props) {
-      super(props);
-      this.state = {
+        super(props);
+
+    this.state = {
         randomPOS: this.partsOfSpeech[Math.floor(Math.random() * this.partsOfSpeech.length)],
         targetScore: Math.floor(Math.random() * (15 - 7)) + 7,
         lettersGuessedArray: [],
-        //ishidden: false,
         winbtnhidden: true,
         lossbtnhidden: true,
         resultsMessage: "Fill in the blanks with letters that add up to the target score. Your word must match the part of speech as well!",
@@ -60,14 +39,16 @@ export default class Game extends React.Component{
         userWordValue: 0,
         totalUserScore: 0,
         scoreThisRound: 0,
-        secondsLeft: 15,
+        secondsLeft: 30,
         timer: null,
+        error: null,
         userround: 0,
         userscore: 0,
         username: '',
         items: [],
         user: null
-      }
+    };
+
     this.handleChange = this.handleChange.bind(this); 
     this.handleSubmit = this.handleSubmit.bind(this); 
     this.login = this.login.bind(this);
@@ -86,7 +67,7 @@ export default class Game extends React.Component{
         };
     };
 
-    componentDidMount() { //component = game, in this case
+    componentDidMount() {
         let timer = setInterval(this.timeOut, 1000);
         this.setState({ timer });
         auth.onAuthStateChanged((user) => {
@@ -113,8 +94,6 @@ export default class Game extends React.Component{
         });
     };
 
-    // POSIndex = this.partsOfSpeech.indexOf(this.state.randomPOS);
-    // abbreviatedRandomPOS = this.abbreviatedPOSArray[this.POSIndex];
 
     letterClick = (event) => {
         event.preventDefault();
@@ -129,7 +108,6 @@ export default class Game extends React.Component{
         newScoreArray.push(letterScore);
 
         function parse(item) {
-            //parse runningScoreArray to integers, then calculate total
             var parsed = parseInt(item);
             return parsed;
         };
@@ -144,6 +122,7 @@ export default class Game extends React.Component{
             userWordValue: total
         });
     };
+
 
     clear = (event) => {
         event.preventDefault();
@@ -156,6 +135,7 @@ export default class Game extends React.Component{
         });
     };
 
+
     backspace = (event) => {
         event.preventDefault();
         var newLetterArray = this.state.lettersGuessedArray;
@@ -167,7 +147,6 @@ export default class Game extends React.Component{
         newScoreArray.pop();
 
         function parse(item) {
-            //parse runningScoreArray to integers, then calculate total
             var parsed = parseInt(item);
             return parsed;
         };
@@ -183,129 +162,103 @@ export default class Game extends React.Component{
         });
     };
 
-    quit = () => {
-        console.log("Thanks for playing! Come back soon.")
-    };
-
 
     win = () => {
         var newWinningScore = this.state.secondsLeft * 10;
-
-        // this.setState({ishidden: !this.state.ishidden})
-
         this.setState({ scoreThisRound: newWinningScore });
-        //add round's score to total score AND POST TO DB/LEADERBOARD!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         var newTotalScore = this.state.totalUserScore + newWinningScore;
-        //how to show buttons????????????? 
         this.setState({
-            // ishidden: "false", 
             winbtnhidden: false,
             totalUserScore: newTotalScore,
             resultsMessage: "Congratulations, you won! You scored " + newWinningScore + " points this round. Your total score so far is " + newTotalScore + " points. Would you like to play again?"
         });
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////        
-        //show definition?
-        //give option to play again
-
-        //if want to play again...
-            //generate new POS and target score
-            //move to next level
-            this.nextLevel();
-        //otherwise...
-            //say thanks for playing
-            //push score to db (and leaderboard if applicable)!!!!!!!
-
-    }
+    };
 
 
     loss = () => {
-        //need some kind of message saying they lost
-        //give option to play again
-        //disable all buttons
         this.setState({
             resultsMessage: "Sorry, you lost!",
-            lossbtnhidden: false
+            lossbtnhidden: false,
+            winbtnhidden: true
         });
     };
 
 
-
     submit = (event) => {
-        //disable all buttons!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        var joinedArray = this.state.lettersGuessedArray.join("");
+        event.preventDefault();
+        var joined = this.state.lettersGuessedArray.join("");
+        var joinedArray = joined.toLowerCase();
         clearInterval(this.state.timer);
-
-        // function searchForWord() {
-        //     Words.map(function(thisWord) {
-        //         return thisWord.word;
-        //     }).indexOf(joinedArray);
-        // }        
-        // console.log(searchForWord); //undefined
-
-        //word: results.id; POS: results.lexicalEntries.lexicalCategory
-
-        function checkForWord() {
-            API.doesDefinitionExist(joinedArray) //switch to joinedArray
-            .then(function(res) {
-                if (res) {
-                    console.log(res.data) //IF ERROR CODE IS 404, NO WORD FOUND
-                }
-                else {
-                    console.log("not a word!")
+        var thisPOS = this.state.randomPOS[0].toUpperCase();
+        if (this.state.userWordValue !== this.state.targetScore) {
+            this.loss();
+        } else {
+            if (thisPOS == "A") {
+                var AList = wordList.A;
+                var AIndex = AList.indexOf(joinedArray);
+                if (AIndex > -1) {
+                    this.win();
+                } else {
                     this.loss();
                 }
-            }
-            )
-              .catch(err => console.log(err));
-        };
-        
-
-
-
-        //if userScore !== targetScore, loss
-        if (this.state.userWordValue === this.state.targetScore) { //CHANGE BACK TO !==
-            this.loss();
-        } else { 
-            checkForWord();
-                
-            //if joinedArray -- OR THAT WORD WITHOUT AN S -- isn't found in dictionary, loss
-
-        } //otherwise ...
-        //if part of speech doesn't match, loss        
-
+            } else if (thisPOS == "V") {
+                var VList = wordList.V;
+                var VIndex = VList.indexOf(joinedArray);
+                if (VIndex > -1) {
+                    this.win();
+                } else {
+                    this.loss();
+                }
+            } else if (thisPOS == "N") {
+                var NList = wordList.N;
+                var NIndex = NList.indexOf(joinedArray);
+                if (NIndex > -1) {
+                    this.win();
+                }
+                else {
+                    this.loss();
+                }
+            };
+        }
     };
 
     lostQuit = () => {
         this.setState({
-            resultsMessage: "Thanks for playing! Come back soon."
+            resultsMessage: "Thanks for playing! Come back soon.",
+            lossbtnhidden: true
         });
     };
 
     wonQuit = () => {
         this.setState({
+            winbtnhidden: true,
             resultsMessage: "Thanks for playing! Come back soon."
         });
     };
 
-    nextLevel = () => { //for when user wins and wants to continue to next level
-        var newLevel = this.state.level + 1; //show level they're on (update this.state.level)
+    nextLevel = () => {
+        var newLevel = this.state.level + 1;
+        let timer = setInterval(this.timeOut, 1000);
         this.setState({
+            timer: timer,
+            winbtnhidden: true,
             level: newLevel,
             userWordValue: 0,
+            resultsMessage: "Fill in the blanks with letters that add up to the target score. Your word must match the part of speech as well!",
             lettersGuessedArray: [],
             runningScoreArray: [],
             scoreThisRound: 0,
-            secondsLeft: 15,
+            secondsLeft: 30,
             randomPOS: this.partsOfSpeech[Math.floor(Math.random() * this.partsOfSpeech.length)],
             targetScore: Math.floor(Math.random() * (15 - 7)) + 7
         });
-
     };
+
 
     restartGame = () => {
         window.location.reload();
     };
+
 
     handleChange(e) {
       this.setState({
@@ -320,7 +273,7 @@ export default class Game extends React.Component{
           user: null
         });
       });
-     }
+     };
   
     login() {
       auth.signInWithPopup(provider) 
@@ -330,7 +283,7 @@ export default class Game extends React.Component{
             user
           });
         });
-    }
+    };
   
     handleSubmit(e) {
       e.preventDefault();
@@ -343,24 +296,7 @@ export default class Game extends React.Component{
         avatar: this.state.user.photoURL
       }
       itemsRef.push(item);
-      this.setState({
-        // username: '',
-        // userround: 0,
-        // userscore: 0
-      });
-    }
-
-    // winbtnstyle = {
-    //     display: this.state.winbtnhidden? "none" : "block"
-    // };
-
-    // lossbtnstyle = {
-    //     display: this.state.lossbtnhidden? "none" : "block" //if lossbuttonhidden not set to true, don't show
-    // }; //////////////////////////////////////////////////////////////
-
-//ok so want to render card buttons onto card.js...
-//in game.js, need to pass in card style
-//
+    };
 
 
 
@@ -370,6 +306,7 @@ export default class Game extends React.Component{
                 <Row>
                     <h1 className="header animated fadeInDown delay 3s text-center">HyperWord 2</h1>
                 </Row>
+
                 <div className='container'>
                 {this.state.user ?
                 <button onClick={this.logout}>Logout</button>                
@@ -395,10 +332,20 @@ export default class Game extends React.Component{
                       </form>
                 </section>
               </div>
+
                 <Row>
-                    <Card winbtnstyle={{display: this.state.winbtnhidden? "none" : "block"}} lossbtnstyle={{display: this.state.lossbtnhidden? "none" : "block"}} winbtnhidden={this.state.winbtnhidden} lossbtnhidden={this.state.lossbtnhidden}imgSrc="https://media.giphy.com/media/SIulatisvJhV7KPfFz/giphy.gif" randomPOS={this.state.randomPOS} targetScore={this.state.targetScore} resultsMessage={this.state.resultsMessage} lostPlayAgain={this.restartGame} wonPlayAgain={this.nextLevel} wonQuit={this.wonQuit} lostQuit={this.lostQuit}>
-                    
-                    
+                    <Card
+                    winbtnstyle={{display: this.state.winbtnhidden? "none" : "block"}}
+                    lossbtnstyle={{display: this.state.lossbtnhidden? "none" : "block"}}
+                    winbtnhidden={this.state.winbtnhidden}
+                    lossbtnhidden={this.state.lossbtnhidden}imgSrc="https://media.giphy.com/media/SIulatisvJhV7KPfFz/giphy.gif"
+                    randomPOS={this.state.randomPOS}
+                    targetScore={this.state.targetScore}
+                    resultsMessage={this.state.resultsMessage}
+                    lostPlayAgain={this.restartGame}
+                    wonPlayAgain={this.nextLevel}
+                    wonQuit={this.wonQuit}
+                    lostQuit={this.lostQuit}>
                     </Card>
                 </Row>
                 <Row className="text-center">
@@ -408,13 +355,13 @@ export default class Game extends React.Component{
                     <TotalUserScore totalUserScore={this.state.totalUserScore} />
                 </Row>
                 <Row className="text-center">
-                    <Timer seconds={this.state.secondsLeft}/>
+                    <Timer seconds={this.state.secondsLeft} />
                 </Row>
                 <Row className="text-center">
-                    <AnswerSpace guesses={this.state.lettersGuessedArray.join("")}/>
+                    <AnswerSpace guesses={this.state.lettersGuessedArray.join("")} />
                 </Row>
                 <Row className="text-center">
-                    <UserWordValue score={this.state.userWordValue}/>
+                    <UserWordValue score={this.state.userWordValue} />
                 </Row>
                 <Row className="text-center">
                 <Keyboard letterClick={this.letterClick} clear={this.clear} backspace={this.backspace} submit={this.submit} />
@@ -425,4 +372,5 @@ export default class Game extends React.Component{
         
 };    
 
-// export default Game;
+
+export default Game;
